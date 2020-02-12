9f6107b1bf573b540553674752ceee0769127ce7
Andrey Remesnyk <w8cartman@gmail.com>
2020-02-12 18:17:23 +0200

    add pipeline file for jenkins
    Signed-off-by: Andrey Remesnyk <w8cartman@gmail.com>

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,8 +78,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djanga',
+        'USER': 'jopa',
+        'PASSWORD': 'kal',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
