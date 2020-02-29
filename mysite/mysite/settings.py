2c66a23afaee0279198112e81c25cf9e11a195bf
Andrey Remesnyk <w8cartman@gmail.com>
2020-02-29 19:50:27 +0200

    move project to docker
    Signed-off-by: Andrey Remesnyk <w8cartman@gmail.com>

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -79,13 +79,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
 
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangodb',
-        'USER': 'user1',
-        'PASSWORD': '1234567890',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': 'djangodb',
+        #'USER': 'user1',
+        #'PASSWORD': '1234567890',
+        #'HOST': 'localhost',
+        #'PORT': '5432',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
