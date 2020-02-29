311f376345889b4a2b09cc50bac34aa228a043bb
Andrey Remesnyk <w8cartman@gmail.com>
2020-02-29 17:00:41 +0200

    add simple unittest
    Signed-off-by: Andrey Remesnyk <w8cartman@gmail.com>

--- mysite/mainApp/tests.py
+++ mysite/mainApp/tests.py
@@ -1,5 +1,17 @@
 from django.test import TestCase
-from polls.models import News
+from news.models import Articles
 from django.utils import timezone
 
-# Create your tests here.
+
+class mytest(TestCase):
+	title = "13 iphone fake news"
+	posttext="Инновационный тандем сверхширокоугольной и широкоугольной камер позволяет превратить практически любой кадр в прекрасное фото студийного качества. Не ограничивайте свой творческий потенциал, снимайте при недостаточной освещенности и под разными ракурсами, помещайте в кадр в 4 раза больше видов и впечатлений, запечатлевайте масштабные сцены, портреты и безграничные пейзажи. "
+
+	def create_Articles(self):
+		return Articles.objects.create(title=mytest.title, post=mytest.posttext, date=timezone.now())
+
+	def test_Articles(self):
+		news = self.create_Articles()
+		self.assertTrue(isinstance(news, Articles))
+		self.assertEqual(mytest.title, news.title)
+		self.assertEqual(mytest.posttext, news.post)
